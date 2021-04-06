00678011b18e3f5e49173c50e0ca387577617c1d
s19514tt <s19514tt@sfc.keio.ac.jp>
2021-04-06 20:59:57 +0900

    changed loader regex

--- loader.js
+++ loader.js
@@ -1,14 +1,14 @@
 const vucript = require("vucript");
 
 module.exports = function(source, map) {
-    var matches = source.match(/<script lang=("|')(v|V)ucript("|')>([\s\S]+)<\/script>/);
+    var matches = source.match(/<script +lang=("|')(v|V)ucript("|')>([\s\S]+)<\/script>/);
     if (matches != null && matches.length > 4) {
         var vucriptCode = matches[4];
         var vucriptCodeCompiled = vucript["default"].compile(vucriptCode);
         console.log(vucriptCodeCompiled);
         console.log(vucriptCode);
         source = source.replace(vucriptCode, '\n' + vucriptCodeCompiled);
-        source = source.replace(/<script lang=("|')(v|V)ucript("|')>/g, "<script lang='ts'>");
+        source = source.replace(/<script +lang=("|')(v|V)ucript("|')>/g, "<script lang=$1ts$3>");
         this.callback(null, source, map);
     }
     else {
